a1c3c7bb6f02a79266ee5f60231dafc1e52d57a0
vsal <vij.sekhar@gmail.com>
2026-01-23 01:34:24 +0000

    Making a working data pipeline.

---
+++ pipeline/ingest_data.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+from sqlalchemy import create_engine
+from tqdm.auto import tqdm
+import click
+
+
+# assign data typtes to the fields
+dtype = {
+    "VendorID": "Int64",
+    "passenger_count": "Int64",
+    "trip_distance": "float64",
+    "RatecodeID": "Int64",
+    "store_and_fwd_flag": "string",
+    "PULocationID": "Int64",
+    "DOLocationID": "Int64",
+    "payment_type": "Int64",
+    "fare_amount": "float64",
+    "extra": "float64",
+    "mta_tax": "float64",
+    "tip_amount": "float64",
+    "tolls_amount": "float64",
+    "improvement_surcharge": "float64",
+    "total_amount": "float64",
+    "congestion_surcharge": "float64"
+}
+
+parse_dates = [
+    "tpep_pickup_datetime",
+    "tpep_dropoff_datetime"
+]
+
+
+
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', type=int, default=5432, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database')
+@click.option('--year', type=int, default=2021, help='Year')
+@click.option('--month', type=int, default=1, help='Month')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table')
+@click.option('--chunksize', type=int, default=100000, help='Chunk size')
+@click.command()
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, chunksize):
+    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
+    url = f"{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz"
+
+    engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
+
+    df_iter = pd.read_csv(
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=chunksize)
+
+    first = True
+    for df_chunk in tqdm(df_iter):
+        if first:
+            # create table
+            df_chunk.head(0).to_sql(name=target_table, con=engine, if_exists='replace')
+            first = False
+
+        df_chunk.to_sql(name=target_table, con=engine, if_exists='append')
+        print(len(df_chunk))
+
+if __name__ == '__main__':
+    run()
